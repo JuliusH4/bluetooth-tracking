f128a4b88d2b803090f5d5adeccaf0633c859ebf
JuliusH4 <julius.henle@gmx.de>
2022-04-22 17:48:15 +0200

    update scan.py

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ## About
 This projekt is about tracking devices based on its sending bluetooth signals. Raspberry Pi Zero W are use to track all surounding bluetooth signals and send them with the RSI (Signal strength) to a central hub.
 
+> Received Signal Strength Indication for the last received broadcast from the device. This is an integer value measured in dB, where 0 dB is the maximum (theoretical) signal strength, and more negative numbers indicate a weaker signal. (http://ianharvey.github.io/bluepy-doc/scanentry.html#rssi)
+
 ## Folder structure
 This code is separated into reciver modules (RM) and the hub (HB). The RM are scanning the surounding bluetooth signals and privide a http server. The HB is then requesting the data from the RM with https and combine the data to track the devices. To provide a GUI, the HB is also hosting a webapplication made with [react](https://reactjs.org/).
 
--- reciverModule/scan.py
+++ reciverModule/scan.py
@@ -1,20 +1,26 @@
 from bluepy import btle
+import os
+from datetime import datetime
 
 scanner = btle.Scanner()
 
-def get_rssi(device):
-    return device.rssi
-
-def scan_devices(intervall: float = 5):
+def scan_devices(intervall: float = 5, minRssiValue: int = -80):
+    starttime = datetime.utcnow()
     devices = list(scanner.scan(intervall))
+    endtime = datetime.utcnow()
+    print(f"start: {starttime}, end: {endtime}")
     print(f"Found {len(devices)} devices:")
-    devices.sort(key=get_rssi, reverse=True)
+    devices.sort(key=lambda device : device.rssi, reverse=True)
     for device in devices:
-        print(f"mac: {device.addr}; rssi: {device.rssi}")
+        if device.rssi < minRssiValue:
+            break
+        print(f"mac: {device.addr}; rssi: {device.rssi}; connectable: {device.connectable}")
 
 def main():
+    MAC_ADRESS = os.popen("sudo cat /sys/kernel/debug/bluetooth/hci0/identity").read().split(' ')[0]
+    print(f"This mac adress: {MAC_ADRESS}")
     while True:
-        scan_devices(1)
+        scan_devices()
 
 if __name__ == "__main__":
     main()
