adeb1ba51460bb26bd274b771f3b2938e9fd2e0f
JuliusH4 <julius.henle@gmx.de>
2022-05-20 21:14:26 +0200

    add mqtt support for hub

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -1,20 +1,39 @@
-import express from 'express';
+import express from "express";
+import * as mqtt from "mqtt";
 
+const MQTT_ADDRESS = "";
+const MQTT_TOPIC = "/btt/sensorData";
+
+const client: mqtt.MqttClient = mqtt.connect(MQTT_ADDRESS);
 const app = express();
 
-app.get('/sensor', (req, res) => {
-    const params = req.params
-    res.send("Success");
-})
-
-app.get('/positions', (req, res) => {
-    const positions = {
-        "MAC1": {x: 1, y: 5},
-        "MAC2": {x: -2, y: 3}
+client.on("connect", () => {
+  console.log("MQTT Client Connected");
+  client.subscribe(MQTT_TOPIC, (err: any) => {
+    if (!err) {
+      console.log(`subscribed to channel ${MQTT_TOPIC}`);
     }
-    res.send(positions);
-})
+  });
+});
+
+client.on("message", (topic: any, message: Buffer) => {
+  // message is Buffer
+  console.log(message.toString());
+});
+
+app.get("/sensor", (req, res) => {
+  const params = req.params;
+  res.send("Success");
+});
+
+app.get("/positions", (req, res) => {
+  const positions = {
+    MAC1: { x: 1, y: 5 },
+    MAC2: { x: -2, y: 3 },
+  };
+  res.send(positions);
+});
 
 app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
-})
+  console.log("The application is listening on port 3000!");
+});
