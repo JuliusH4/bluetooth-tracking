9a0641645f92254aa035c7983e98b70bbe53918a
JuliusH4 <julius.henle@gmx.de>
2022-06-22 01:15:56 +0200

    fix

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -22,7 +22,8 @@ client.on("connect", () => {
 });
 
 client.on("message", (topic: string, message_: Buffer) => {
-  const message = JSON.parse(message_.toString())
+  const message_str = message_.toString().replace('\'', '"')
+  const message = JSON.parse(message_str)
   console.info("recived MQTT message", topic, message);
   deviceHandler.setSignals(message);
 });
